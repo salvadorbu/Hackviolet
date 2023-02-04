04ba594f17b586c7e1d0f4367c59a594801120f3
Salvador Buenadicha Calvo <salvadorbu@vt.edu>
2023-02-04 18:09:25 -0500

    update file

--- canvas_api.py
+++ canvas_api.py
@@ -13,17 +13,24 @@ class CanvasBot:
         for assignment in self.canvas.get_todo_items()._get_next_page():
             due_date = str(assignment.assignment['due_at'])
             due_date = f"{due_date[5:7]}/{due_date[8:10]}"
-
+            course_id = assignment.assignment['course_id']
+            assignment_id = assignment.assignment['id']
             url = assignment.assignment['html_url']
 
-            assignments.append((assignment.assignment['name'], due_date, url))
+            assignments.append((assignment.assignment['name'], due_date, url, course_id, assignment_id))
 
         return assignments
+    
+    def submit_assignment(self, course_id, assignment_id, submit_file):
+        course = self.canvas.get_course(course_id)
+        assignment = course.get_assignment(assignment_id)
+        assignment.submit({"submission_type": "online_upload"}, file=submit_file)
 
 
 if __name__ == "__main__":
     url = "https://canvas.vt.edu"
     key = "4511~CSBkFbb31upwYZYNHWfENaVnF0xOdXbGPl9Kr55rDC5M4y3hr0QMx8wkvbLHQIxs"
     bot = CanvasBot(url, key)
-
-    bot.get_assignements()
+    file = open("C:\\Users\\salva\\Downloads\\submission.pdf", "r")
+    ass = bot.get_assignements()
+    bot.submit_assignment(167776, 1690075, file)
