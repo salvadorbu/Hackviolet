83d4a101012a2b4439c2624a77df19ba16bbc5df
Salvador Buenadicha Calvo <salvadorbu@vt.edu>
2023-02-04 16:32:43 -0500

    asefsa

--- canvas_api.py
+++ canvas_api.py
@@ -1,5 +1,6 @@
 from canvasapi import Canvas
-
+import datetime
+from datetime import date
 class CanvasBot:
     def __init__(self, url, key) -> None:
         self.canvas = Canvas(url, key)
@@ -7,13 +8,38 @@ class CanvasBot:
 
     def get_user(self):
         return self.user
+
+    def is_current_semester(self, created_date):
+        year = st[0:4]
+        month = st[5:7]
+        semester = ""
+
+        if month == ("06" or "07"):
+          semester = "Fall"
+        else:
+            semester = "Spring"
+            year = str(int(year))
+        return self.sem_to_string(str(date.today())) == self.sem_to_string(created_date)
     
-    def _get_courses(self):
-        return self.canvas.get_courses()._get_next_page()
-            
+    def get_courses(self):
+        paginated = self.user.get_courses()._get_next_page()
+        courses = []
+        for p in paginated:
+            try:
+                courses.append((str(p.created_at_date), p.id))
+            except:
+                pass
+        return courses
+        
+
+    def get_assignements(self):
+        assignments = self.canvas.get_todo_items()._get_next_page()
+        
+
 
 if __name__ == "__main__":
     url = "https://canvas.vt.edu"
     key = "4511~CSBkFbb31upwYZYNHWfENaVnF0xOdXbGPl9Kr55rDC5M4y3hr0QMx8wkvbLHQIxs"
     bot = CanvasBot(url, key)
-    print(bot.get_courses())
+
+    bot.get_assignements()
